8f9051016b69c482788eb0dd5363f4671a32c455
Your name <you@email.com>
2013-08-01 21:06:46 -0500

    Nothing in Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentClasses::Application.config.secret_key_base = '8fa91cb883a8780407814284ec6dbac5d5f2afde1d4d607431b2630f8487937cb3688dfb999f156e33fafd6deb8fa474dafbde358a4be93ce967aeb60d06799d'
